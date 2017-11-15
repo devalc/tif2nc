2f2364f0ed1a08a3192c3afda12f7b06e61f8c8f
devalc <deva4998@vandals.uidaho.edu>
2017-11-14 16:15:54 -0800

    Update tiftonc.sh

--- tiftonc.sh
+++ tiftonc.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-
+############################################################################################################
+# -*- coding: utf-8 -*-
+#Created on Fri Mar 3 09:08:42 2017
+#@author: chinmay deval
 #######script writes all the tiff files to a netcdf file using CDO functionality ######
-##### The example input Tiff file name is as: ETensemble_mm-month-1_2012.02.01.tif ##### 
+##### The example input Tiff file name is as: ETensemble_mm-month-1_2012.02.01.tif #####
+############################################################################################################
 
 echo "Listing all extracted files"
 ls -al
